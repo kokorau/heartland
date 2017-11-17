098e9b570ffd47bc137e2ca6297411c992bdf34f
kokorau <kokorau@gmail.com>
2017-11-17 16:08:53 +0900

    とりあえずローンチ版最終調整

--- src/front/FrDescription.vue
+++ src/front/FrDescription.vue
@@ -3,15 +3,19 @@
     h3.section-title INFOMATION
 
     el-card.description
-      p
-        label LOCATION:
-        a(target="_blank",
-          href="https://www.google.co.jp/maps/place/OATH/@35.6582777,139.7092371,19z/data=!4m5!3m4!1s0x60188b5e7f0c8ba1:0xc268e385918a1532!8m2!3d35.6586473!4d139.7091919")
-          |AOYAMA OATH
-      p
-        label DATE:
-        |2017/12/17 18:00-24:00
-
+      ul
+        li
+          label LOCATION:
+          a(target="_blank",
+            href="https://www.google.co.jp/maps/place/OATH/@35.6582777,139.7092371,19z/data=!4m5!3m4!1s0x60188b5e7f0c8ba1:0xc268e385918a1532!8m2!3d35.6586473!4d139.7091919"
+          ) AOYAMA OATH
+            icon(name="map-marker", :scale="1")
+        li
+          label DATE: 2017/12/17 18:00-24:00
+        li
+          label ENTRANCE: FREE
+        li
+          label FIRST DRINK: 1,000 YEN
 </template>
 
 <script>
@@ -36,7 +40,10 @@
     border-radius: 3px
     margin-bottom: 1.5px
 
-    p
+    li
+      list-style-type: none
+
+    p, label, a
       font-size: 200%
 
   .el-card__body
